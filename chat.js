1ae4eb4c731baf4247ba6ac5453a7a6d36b94ab8
Jonghyuk Lee <whdgur826333@gmail.com>
2023-04-11 03:03:20 -0600

    minor changes for chat

--- chat.js
+++ chat.js
@@ -1,3 +1,11 @@
+// const chatControls = document.querySelector('#chat-container');
+// const myName = document.querySelector('#my-name');
+// // myName.addEventListener('keyup', (e) => {
+// //   chatControls.disabled = myName.value === '';
+// // });
+function nameActive() {
+    document.getElementById('chat-container').disabled = false;
+}
 function sendmessage() {
-    
+
 }
