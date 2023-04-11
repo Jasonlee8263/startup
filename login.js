fe7e043a39d43bc32b267c1b783d865538871a8f
Jonghyuk Lee <whdgur826333@gmail.com>
2023-04-10 23:43:49 -0600

    complete login and create part

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function login() {
-    const nameEI = document.getElementById('userName')
-    const passwordEI = document.getElementById('userPassword')
+    const nameEI = document.querySelector('#userName')
+    const passwordEI = document.querySelector('#userPassword')
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     if (nameEI.value == '' || passwordEI.value == '') {
         alert('Please enter both username and password.')
@@ -8,4 +8,24 @@ function login() {
     else if (!emailRegex.test(nameEI.value)) {
         alert('Please enter a valid email address.')
     }
+}
+
+function create() {
+    const nameEI = document.querySelector('#userName')
+    const passwordEI = document.querySelector('#userPassword')
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (nameEI.value == '' || passwordEI.value == '') {
+        alert('Please enter both username and password.')
+    }
+    else if (!emailRegex.test(nameEI.value)) {
+        alert('Please enter a valid email address.')
+    }
+    else {
+        alert('Sign up is complete.')
+        window.location.href='signin.html'
+    }
+}
+
+function authentication() {
+    
 }
